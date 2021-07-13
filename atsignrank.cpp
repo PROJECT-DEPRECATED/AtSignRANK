51eb661303af286ddc26b2c13036543c63ae7724
RANK <81941006+AtSignRANK@users.noreply.github.com>
2021-07-13 21:45:42 +0900

    Create atsignrank.cpp

---
+++ atsignrank.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+char atsignrank[] = "At Sign RANK";
+bool isRANK_idiot = true;
+int main() {
+  printf("Name : %s", atsignrank);
+  printf("Born : %.4lf", 2008.1104);
+  printf("RANK == idiot : %d", isRANK_idiot);
+  return 0;
+}
